09deccc9cad6847b4aad1ed2c1a9443daa7e6d44
Kaoline <jujulableue@hotmail.com>
2022-09-17 23:51:23 +0200

    Implement GetAvailableCurrenciesUseCase

--- app/src/main/java/kaoline/converter/domain/GetAvailableCurrenciesUseCase.kt
+++ app/src/main/java/kaoline/converter/domain/GetAvailableCurrenciesUseCase.kt
@@ -1,12 +1,20 @@
 package kaoline.converter.domain
 
+import kaoline.converter.domain.model.Amount
 import kaoline.converter.utils.Result
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 
 class GetAvailableCurrenciesUseCase(
     private val ratesRepository: IRatesRepository
 ) : IGetAvailableCurrenciesUseCase {
     override fun performAction(): Flow<Result<List<String>>> = flow {
+        emit(Result.Loading)
+        val currencies = ratesRepository.getRates().map { it.toCurrency }.toMutableList()
+            .apply { if (!contains(Amount.USD)) add(Amount.USD) }
+        emit(Result.Success(currencies))
+    }.catch {
+        emit(Result.Failure(it))
     }
 }
